f9b8413b4fdf8ef7b278d05465e0f25f17cdac45
Niklas Salmoukas <niklas@salmoukas.com>
2017-01-17 02:19:37 +0100

    first draft

---
+++ src/module.js
@@ -0,0 +1,120 @@
+/**
+ * @copyright 2015, Andrey Popp <8mayday@gmail.com>
+ * @copyright 2017, Niklas Salmoukas <niklas@salmoukas.com>
+ *
+ * The decorator may be used on classes or methods
+ * ```
+ * @bindThis
+ * class FullBound {}
+ *
+ * class PartBound {
+ *   @bindThis
+ *   method () {}
+ * }
+ * ```
+ */
+export default function bindThis(...args) {
+  if(  args.length === 1
+    && typeof args[0] === 'function'
+  ) {
+    return bindClassMethods(...args);
+  }
+  else
+  if(  args.length === 1
+    && typeof args[0] === 'object'
+  ) {
+    return bindObjectMethods(...args);
+  }
+  else
+  if(   args.length === 3
+    &&  typeof args[0] === 'object'
+    && (typeof args[1] === 'string' || args[1] instanceof Symbol)
+    &&  typeof args[2] === 'object'
+  ) {
+    return bindMethod(...args);
+  }
+  else {
+    throw new Error('invalid parameters');
+  }
+}
+
+function bindClassMethods(target) {
+  // bind methods on prototype object
+  bindObjectMethods(target.prototype);
+  return target;
+}
+
+function bindObjectMethods(target) {
+  // retrieve all keys
+  let keys;
+  if (typeof Reflect !== 'undefined' && typeof Reflect.ownKeys === 'function') {
+    keys = Reflect.ownKeys(target);
+  } else {
+    keys = Object.getOwnPropertyNames(target);
+    // use symbols if support is provided
+    if (typeof Object.getOwnPropertySymbols === 'function') {
+      keys = keys.concat(Object.getOwnPropertySymbols(target));
+    }
+  }
+  // apply function binding
+  keys.forEach(key => {
+    // Ignore constructor
+    if (key === 'constructor') {
+      return;
+    }
+    // bind function
+    let descriptor = Object.getOwnPropertyDescriptor(target, key);
+    if (typeof descriptor.value === 'function') {
+      Object.defineProperty(
+        target, key,
+        bindMethod(target, key, descriptor)
+      );
+    }
+  });
+  return target;
+}
+
+function bindMethod(target, key, descriptor) {
+  // validate value
+  let fn = descriptor.value;
+  if (typeof fn !== 'function') {
+    throw new Error(`@bindThis decorator can only be applied to methods not: ${typeof fn}`);
+  }
+  // in IE11 calling Object.defineProperty has a side-effect of evaluating the
+  // getter for the property which is being replaced. This causes infinite
+  // recursion and an "Out of stack space" error.
+  let definingProperty = false;
+  // generate new descriptor
+  return {
+    configurable: true,
+    enumerable: true,
+    get() {
+      // bind function and return if memoization is not possible
+      let boundFn = fn.bind(this);
+      if (definingProperty || this === target || this.hasOwnProperty(key)) {
+        return boundFn;
+      }
+      // memoize bound function and return
+      definingProperty = true;
+      Object.defineProperty(this, key, {
+        value: boundFn,
+        configurable: true,
+        enumerable: true,
+        writable: true
+      });
+      definingProperty = false;
+      return boundFn;
+    },
+    set(value) {
+      // override property
+      definingProperty = true;
+      Object.defineProperty(this, key, {
+        value: value,
+        configurable: true,
+        enumerable: true,
+        writable: true
+      });
+      definingProperty = false;
+    }
+  };
+}
